8e7c342323e7b0a3824ebd554bdc07cd39969244
optimus1977 <dragos.grosu29@gmail.com>
2024-11-13 09:58:40 +0000

    Player Update
    Move and animation for player

---
+++ scripts/player.gd
@@ -0,0 +1,36 @@
+extends CharacterBody2D
+
+
+const speed = 300.0
+var player_state
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+	#move player 
+	var direction = Input.get_vector("left", "right", "up", "down")
+	
+	if direction.x == 0 and direction.y == 0:
+		player_state = "idle"
+	elif direction.x != 0 or direction.y != 0:
+		player_state = "walking"
+		
+	velocity = direction * speed
+	move_and_slide()
+	#play the correct animation
+	play_anim(direction)
+
+func play_anim(dir):
+	if player_state == "idle":
+		$AnimatedSprite2D.play("idle")
+	if player_state == "walking":
+		if dir.y == -1:
+			$AnimatedSprite2D.play("move_up")
+		if dir.x == 1:
+			$AnimatedSprite2D.play("move_right")
+		if dir.y == 1:
+			$AnimatedSprite2D.play("move_down")
+		if dir.x == -1:
+			$AnimatedSprite2D.play("move_left")
