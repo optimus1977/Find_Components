1deddfc9c69874f9c163bb328b8095970bf01679
optimus1977 <dragos.grosu29@gmail.com>
2024-11-14 20:53:15 +0000

    UI for collectable

---
+++ scripts/cpu_ui.gd
@@ -0,0 +1,11 @@
+extends Control
+
+@onready var label = $Label
+
+func _ready():
+	EventController.connect("cpu_collected", on_event_cpus_collected)
+
+#assing the value to label
+func  on_event_cpus_collected(value: int) -> void:
+	label.text = str(value)
+
